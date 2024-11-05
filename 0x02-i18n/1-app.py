2e65acbb7d6d29d9c0a70997919ee48813772ea0
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-11-05 21:51:04 +0000

    i18n and l10n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''set up flaska-babel'''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    '''configuration class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello():
+    '''render template to html file'''
+    return render_template('1-index.html')
